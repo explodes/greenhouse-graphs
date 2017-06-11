5a8d659f80894349b61c58dcafed388a6492a021
Evan Leis <evan.explodes@gmail.com>
2017-06-10 23:19:05 -0600

    [extents] show extends of the graph

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -1,5 +1,13 @@
 <template>
-  <svg :class="graphClass" width="500" height="270"></svg>
+  <div>
+    <svg :class="graphClass" width="500" height="270">
+      <g style="transform: translate(0, 10px)">
+        <path :d="line"></path>
+      </g>
+    </svg>
+    <p>Temperature: Min {{ temperature.min }} Max: {{ temperature.max }}</p>
+    <p>Date range: Min {{ dates.min }} Max: {{ dates.max }}</p>
+  </div>
 </template>
 <script>
   import {Greenhouse} from '../lib/greenhouseapi'
@@ -20,6 +28,9 @@
         greenhouse: greenhouse,
         data: [],
         updateTimer: null,
+        line: '',
+        temperature: {min: 0, max: 0},
+        dates: {min: 0, max: 0}
       };
     },
     mounted() {
@@ -57,35 +68,32 @@
             this.calculatePath();
           });
       },
-      calculatePaddedTemperatureExtent() {
+      calculateExtents() {
         let yExtent = d3.extent(this.data, d => d.fahrenheit);
         const [min, max] = yExtent;
-        return [
+        yExtent = [
           min - (max - min) * .1,
           max + (max - min) * .1,
         ];
+        [this.temperature.min, this.temperature.max] = yExtent;
+        const xExtent = d3.extent(this.data, d => d.when);
+        [this.dates.min, this.dates.max] = xExtent;
+        return [xExtent, yExtent];
       },
       buildScales() {
-        const x = d3.scaleTime().range([0, 500]).domain(d3.extent(this.data, d => d.when));
-        const y = d3.scaleLinear().range([270, 0]).domain(this.calculatePaddedTemperatureExtent());
+        const [xExtent, yExtent] = this.calculateExtents();
+        const x = d3.scaleTime().range([0, 500]).domain(xExtent);
+        const y = d3.scaleLinear().range([270, 0]).domain(yExtent);
         d3.axisLeft().scale(x);
         d3.axisBottom().scale(y);
         return {x, y};
       },
       calculatePath() {
-        const graph = d3.select(`.${this.graphClass}`);
-        const {x:scaleX, y:scaleY} = this.buildScales();
-        // tear down
-        graph.selectAll('path').remove();
-        // build up
+        const {x: scaleX, y: scaleY} = this.buildScales();
         const path = d3.line()
           .x(d => scaleX(d.when))
           .y(d => scaleY(d.fahrenheit));
-        graph.append('path')
-          .attr('stroke-width', '3px')
-          .attr('stroke', '#76BF8A')
-          .attr('fill', 'none')
-          .attr('d', path(this.data));
+        this.line = path(this.data);
       },
     },
   };
@@ -94,4 +102,8 @@
   svg.temp-graph
     background-color: #432
     margin: 25px
+    path
+      stroke-width: 3px
+      stroke: #76bf8a
+      fill: none
 </style>
